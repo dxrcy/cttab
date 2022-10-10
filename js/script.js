46203af50671605f5bd9a1532b89c3723c778619
DarccMan <darcy3057@gmail.com>
2022-10-11 09:28:50 +1100

    Move ls object to class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-// DOCUMENTATION - https://github.com/darccyy/cttab#cttab
-// Reload
+//* DOCUMENTATION - https://github.com/darccyy/cttab#cttab
+
+// Reload all states
 function init() {
   ls.check();
   console.log(language.get("console"));
@@ -24,76 +25,84 @@ function getTemplate(name, values) {
   );
 }
 
-// Local Storage
-const ls = {};
-ls.check = function () {
-  if (localStorage.cttab) {
-    try {
-      JSON.parse(localStorage.cttab);
-    } catch (err) {
+// Local Storage API
+class ls {
+  // Check if localStorage exists and is valid, otherwise reset
+  static check() {
+    if (localStorage.cttab) {
+      try {
+        JSON.parse(localStorage.cttab);
+      } catch (err) {
+        ls.reset();
+      }
+    } else {
       ls.reset();
     }
-  } else {
-    ls.reset();
   }
-};
 
-ls.reset = function () {
-  localStorage.cttab = JSON.stringify({
-    header: null,
-    sc: { amount: sc.default, array: {} },
-    search: null,
-    notes: [""],
-    lang: "en",
-    bg: { color: null, image: null },
-    garf: false,
-    cache: {},
-  });
-
-  init();
-};
-
-ls.resetConfirm = function () {
-  if (!confirm(language.get("ls_reset"))) {
-    return;
-  }
-  ls.reset();
-  location.reload();
-};
-
-ls.__defineGetter__("all", () => {
-  ls.check();
-  return JSON.parse(localStorage.cttab);
-});
-
-ls.set = function (callback) {
-  all = ls.all;
-  const value = callback(all);
-  if (value) {
-    localStorage.cttab = value;
-    return;
-  }
-  localStorage.cttab = JSON.stringify(all);
-};
-
-const oFReader = new FileReader();
-ls.import = async function (data) {
-  if (!data) {
-    // Read file
-    data = await new Promise(resolve => {
-      oFReader.readAsText($("#ls-file")[0].files[0]);
-      oFReader.onload = function (oFREvent) {
-        resolve(oFREvent.target.result);
-      };
+  // Reset localStorage
+  static reset() {
+    localStorage.cttab = JSON.stringify({
+      header: null,
+      sc: { amount: sc.default, array: {} },
+      search: null,
+      notes: [""],
+      lang: "en",
+      bg: { color: null, image: null },
+      garf: false,
+      cache: {},
     });
-  }
-  ls.set(() => data);
-  init();
-};
 
-ls.export = function () {
-  download(JSON.stringify(ls.all), "cttab-data.json", "text/json");
-};
+    init();
+  }
+
+  // Reset with confirmation box
+  static resetConfirm() {
+    if (!confirm(language.get("ls_reset"))) {
+      return;
+    }
+    ls.reset();
+    location.reload();
+  }
+
+  // Get all localStorage as JSON
+  static get all() {
+    ls.check();
+    return JSON.parse(localStorage.cttab);
+  }
+
+  // Set localStorage with callback
+  static set(callback) {
+    var all = ls.all;
+    const value = callback(all);
+    if (value) {
+      localStorage.cttab = value;
+      return;
+    }
+    localStorage.cttab = JSON.stringify(all);
+  }
+
+  // Import localStorage from file
+  static oFReader = new FileReader();
+  static async import(data) {
+    if (!data) {
+      // Read file
+      data = await new Promise(resolve => {
+        this.oFReader.readAsText($("#ls-file")[0].files[0]);
+        this.oFReader.onload = function (oFREvent) {
+          resolve(oFREvent.target.result);
+        };
+      });
+    }
+    ls.set(() => data);
+    init();
+  }
+
+  // Export localStorage to file
+  static export() {
+    download(JSON.stringify(ls.all), "cttab-data.json", "text/json");
+  }
+}
 
 // Download file
 function download(data, filename, type) {
